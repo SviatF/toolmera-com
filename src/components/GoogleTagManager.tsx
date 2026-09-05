ee7cc66f27d6981bcf9a1ca140d1a1e4ff02cd8d
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-05 13:45:47 -0700

    analytics: add GTM with admin exclusion

---
+++ src/components/GoogleTagManager.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Script from 'next/script';
+import { usePathname } from 'next/navigation';
+
+const GTM_ID = 'GTM-P9P7456H';
+
+export function GoogleTagManager() {
+  const pathname = usePathname();
+  const isAdmin = pathname === '/admin' || pathname?.startsWith('/admin/');
+
+  if (isAdmin) return null;
+
+  return (
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+          title="Google Tag Manager"
+        />
+      </noscript>
+    </>
+  );
+}
