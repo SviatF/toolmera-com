1b1e03a26b8a35307d1480d08c7c6f6fcff7921f
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-16 04:08:52 -0700

    Add German localized tools hub

---
+++ src/app/de/page.tsx
@@ -0,0 +1,4 @@
+import { LocalizedPilotHub, localizedHubMetadata } from '@/components/LocalizedPilotPage';
+
+export const metadata=localizedHubMetadata('de');
+export default function GermanHub(){return <LocalizedPilotHub locale="de"/>}
