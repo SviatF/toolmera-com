812547117c506f0ce4f52d4242fcf09f5c274e27
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-16 04:09:25 -0700

    Add Russian localized tools hub

---
+++ src/app/ru/page.tsx
@@ -0,0 +1,4 @@
+import { LocalizedPilotHub, localizedHubMetadata } from '@/components/LocalizedPilotPage';
+
+export const metadata=localizedHubMetadata('ru');
+export default function RussianHub(){return <LocalizedPilotHub locale="ru"/>}
