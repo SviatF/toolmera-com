9d68d119a9617d50fb3d7c8696edbad84fb11635
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-16 04:09:10 -0700

    Add Hindi localized tools hub

---
+++ src/app/hi/page.tsx
@@ -0,0 +1,4 @@
+import { LocalizedPilotHub, localizedHubMetadata } from '@/components/LocalizedPilotPage';
+
+export const metadata=localizedHubMetadata('hi');
+export default function HindiHub(){return <LocalizedPilotHub locale="hi"/>}
