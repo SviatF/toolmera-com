b574d23a9fbeca6a750bc1476f5a4cfc5e80876e
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-24 10:00:55 -0700

    Expand German localized hub

--- src/app/de/page.tsx
+++ src/app/de/page.tsx
@@ -1,4 +1,4 @@
-import { LocalizedPilotHub, localizedHubMetadata } from '@/components/LocalizedPilotPage';
+import { LocalizedScaleHub, localizedScaleHubMetadata } from '@/components/LocalizedScalePage';
 
-export const metadata=localizedHubMetadata('de');
-export default function GermanHub(){return <LocalizedPilotHub locale="de"/>}
+export const metadata=localizedScaleHubMetadata('de');
+export default function GermanHub(){return <LocalizedScaleHub locale="de"/>}
